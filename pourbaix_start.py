ca2e46dc07ff2672620be4b86e28d2804c1cbec6
Pålly <pal.karstensen@sintef.no>
2024-06-11 08:54:46 +0200

    Update pourbaix_start
    Added all the thermodynamic data needed to add the temperature dependence. The idea is to use Gibbs free energy, but that might be a bit too simple perhaps?

--- pourbaix_start.py
+++ pourbaix_start.py
@@ -9,12 +9,15 @@ constants = {
 'F': 96485    # [C/mol]
 }
 
+## THERMODYNAMIC DATA ##
+
 # Gibbs free energy of formation of species at STP [REVISED POURBAIX DIAGRAM]
 constants_deltaG_formation = {
 'deltaG_Zn': 0,                     # Zn(s)
 'deltaG_Zn^2+': -147.203*10**3,     # Zn^2+(aq)
 'deltaG_Zn(OH)^+': -333.20*10**3,   # Zn(OH)^+(aq)
-'deltaG_Zn(OH)2': -555.82*10**3,    # Zn(OH)2(s)
+'deltaG_Zn(OH)2': -525.02*10**3,    # Zn(OH)2(s)  ----  
+'deltaG_Zn(OH)2-eps':-555.82*10**3,  # Solid form
 'deltaG_Zn(OH)3^-': -696.52*10**3,  # Zn(OH)3^-(aq)
 'deltaG_Zn(OH)4^2-': -860.59*10**3, # Zn(OH)4^2-(aq)
 'deltaG_ZnO': -320.479*10**3,       # ZnO
@@ -23,8 +26,104 @@ constants_deltaG_formation = {
 'deltaG_OH^-':-157.2*10**3,         # OH^-(aq)
 }
 
+# Entropy of formation of species at STP [REBVISED POURBAIX DIAGRAM]
+constants_S_formation = {
+'S_Zn': 41.63,          # Zn(s)
+'S_Zn^2+': -109.8,      # Zn^2+(aq)
+'S_Zn(OH)^+': -24,      # Zn(OH)^+(aq)
+'S_Zn(OH)2': 43,        # Zn(OH)2(s)
+'S_Zn(OH)2-eps': 77,
+'S_Zn(OH)3^-': 40,      # Zn(OH)3^-(aq)
+'S_Zn(OH)4^2-': 15,     # Zn(OH)4^2-(aq)
+'S_ZnO': 43.65,         # ZnO
+'S_H^+': 0,             # H^+(aq)
+'S_H2O': 70,  # H2O
+'S_OH^-':-11,  # OH^-(aq)
+}
+
+# Molar heat capacity of species at STP [REVISED POURBAIX DIAGRAM]
+constants_Cp = {
+'Cp_Zn': [21.334, 11.648*10**(-3), 0.054*10**(6)],
+'Cp_ZnO':[45.338, 7.289*10**(-3), -0.573*10**(6)],
+'Cp_Zn(OH)2-eps':74.27,
+'Cp_Zn^2+':-25.8,
+'Cp_Zn(OH)^+': 10,
+'Cp_Zn(OH)2': 70,
+'Cp_Zn(OH)3^-': 94,
+'Cp_Zn(OH)4^2-': -284,
+'Cp_H^+': 0,
+'Cp_H2O': 75,
+'Cp_OH^-': -149
+}
+
+
+
+equilibrium_constants_2 = {'Zn(OH)': 10**4.4, 'ZnOH2_aq': 10**11.3, 'Zn(OH)3': 10**14.14, 'Zn(OH)4': 10**17.66,\
+                            'Zn(NH3)': 10**2.37, 'Zn(NH3)2': 10**4.81, 'Zn(NH3)3': 10**7.31, 'Zn(NH3)4': 10**9.46,\
+                            'Zn(NH3)(OH)': 10**9.23, 'Zn(NH3)2(OH)': 10**10.80, 'Zn(NH3)3(OH)': 10**12,  'Zn(NH3)(OH)2_aq': 10**13, 'Zn(NH3)2(OH)2_aq': 10**13.6, 'Zn(NH3)(OH)3': 10**14.50,\
+                            'ZnOH2_sat': 10**(-14.82), 'ZnO': 10**(-15.96), 'ZnCO3': 10**(-10), \
+                            'H2CO3': 10**(6.33), 'HCO3': 10**(9.56), 'pCO2': 10**(-1.55),\
+                            'NH3': 10**(-9.246)}  # Initialize with a non-zero value
+
 ## Calculating E0 from thermodynamic data
 
+# Gibbs free energy for reaction st STP
+constants_deltaG = {
+'deltaG_I': constants_deltaG_formation['deltaG_Zn'] - constants_deltaG_formation['deltaG_Zn^2+'],                                                          # Zn^2+ + 2e^- <--> Zn(s)
+'deltaG_II': constants_deltaG_formation['deltaG_Zn'] + constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)^+']) + constants_deltaG_formation['deltaG_H^+'],      # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O
+'deltaG_III': constants_deltaG_formation['deltaG_Zn'] + 2*constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)2-eps']+ 2*constants_deltaG_formation['deltaG_H^+']),   # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O
+'deltaG_IV': constants_deltaG_formation['deltaG_Zn'] + 3*constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)3^-'] + 3*constants_deltaG_formation['deltaG_H^+']), # Zn(OH)2 + 2e^- + 2H^+ <--> Zn(s) + 2H2O
+'deltaG_V': constants_deltaG_formation['deltaG_Zn'] + 4*constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)4^2-'] + 4*constants_deltaG_formation['deltaG_H^+']), # Zn(OH)4^2- + 2e^- + 4H^+ <--> Zn(s) + 4H2O
+'deltaG_VI': constants_deltaG_formation['deltaG_Zn'] + constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_ZnO']) + constants_deltaG_formation['deltaG_H^+'],           # ZnO + 2e^- + 2H^+ <--> Zn + H2O
+'deltaG_VIII': constants_deltaG_formation['deltaG_Zn(OH)2-eps'] - (constants_deltaG_formation['deltaG_Zn^2+'] + 2*constants_deltaG_formation['deltaG_OH^-']),  # [J/mol] - Zn^2+ + 2OH^- <--> Zn(OH)2
+'deltaG_IX': constants_deltaG_formation['deltaG_Zn(OH)3^-'] - (constants_deltaG_formation['deltaG_Zn(OH)2-eps'] + constants_deltaG_formation['deltaG_OH^-']),  # [J/mol] - Zn(OH)2 + OH^- <--> Zn(OH)3^-
+'deltaG_X': constants_deltaG_formation['deltaG_Zn(OH)4^2-'] - (constants_deltaG_formation['deltaG_Zn(OH)3^-'] + constants_deltaG_formation['deltaG_OH^-']),# [J/mol] - Zn(OH)3^- + OH^- <--> Zn(OH)4^-2
+'deltaG_XI': constants_deltaG_formation['deltaG_Zn(OH)4^2-'] - (constants_deltaG_formation['deltaG_Zn(OH)2-eps'] + 2*constants_deltaG_formation['deltaG_OH^-'])# [J/mol] - Zn(OH)2 +2OH^- <--> Zn(OH)4^2-
+}
+
+# Entropy of reaction at STP
+constants_deltaS = {
+'deltaS_I': constants_S_formation['S_Zn'] - constants_S_formation['S_Zn^2+'],                                                          # Zn^2+ + 2e^- <--> Zn(s)
+'deltaS_II': constants_S_formation['S_Zn'] + constants_S_formation['S_H2O'] - (constants_S_formation['S_Zn(OH)^+']) + constants_S_formation['S_H^+'],      # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O
+'deltaS_III': constants_S_formation['S_Zn'] + 2*constants_S_formation['S_H2O'] - (constants_S_formation['S_Zn(OH)2']+ 2*constants_S_formation['S_H^+']),   # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O
+'deltaS_IV': constants_S_formation['S_Zn'] + 3*constants_S_formation['S_H2O'] - (constants_S_formation['S_Zn(OH)3^-'] + 3*constants_S_formation['S_H^+']), # Zn(OH)2 + 2e^- + 2H^+ <--> Zn(s) + 2H2O
+'deltaS_V': constants_S_formation['S_Zn'] + 4*constants_S_formation['S_H2O'] - (constants_S_formation['S_Zn(OH)4^2-'] + 4*constants_S_formation['S_H^+']), # Zn(OH)4^2- + 2e^- + 4H^+ <--> Zn(s) + 4H2O
+'deltaS_VI': constants_S_formation['S_Zn'] + constants_S_formation['S_H2O'] - (constants_S_formation['S_ZnO']) + constants_S_formation['S_H^+'],           # ZnO + 2e^- + 2H^+ <--> Zn + H2O
+'deltaS_VIII': constants_S_formation['S_Zn(OH)2'] - (constants_S_formation['S_Zn^2+'] + 2*constants_S_formation['S_OH^-']),  # [J/mol] - Zn^2+ + 2OH^- <--> Zn(OH)2
+'deltaS_IX': constants_S_formation['S_Zn(OH)3^-'] - (constants_S_formation['S_Zn(OH)2'] + constants_S_formation['S_OH^-']),  # [J/mol] - Zn(OH)2 + OH^- <--> Zn(OH)3^-
+'deltaS_X': constants_S_formation['S_Zn(OH)4^2-'] - (constants_S_formation['S_Zn(OH)3^-'] + constants_S_formation['S_OH^-']),# [J/mol] - Zn(OH)3^- + OH^- <--> Zn(OH)4^-2
+'deltaS_XI': constants_S_formation['S_Zn(OH)4^2-'] - (constants_S_formation['S_Zn(OH)2'] + 2*constants_S_formation['S_OH^-'])# [J/mol] - Zn(OH)2 +2OH^- <--> Zn(OH)4^2-
+}
+
+# Enthalpy for reaction as STP
+constants_deltaH = {
+'deltaH_I': constants_deltaG['deltaG_I'] + constants['T']*constants_deltaS['deltaS_I'],         # Zn^2+ + 2e^- <--> Zn(s)
+'deltaH_II': constants_deltaG['deltaG_II'] + constants['T']*constants_deltaS['deltaS_II'],      # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O
+'deltaH_III': constants_deltaG['deltaG_III'] + constants['T']*constants_deltaS['deltaS_III'],   # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O
+'deltaH_IV': constants_deltaG['deltaG_IV'] + constants['T']*constants_deltaS['deltaS_IV'],      # Zn(OH)2 + 2e^- + 2H^+ <--> Zn(s) + 2H2O
+'deltaH_V': constants_deltaG['deltaG_V'] + constants['T']*constants_deltaS['deltaS_V'],         # Zn(OH)4^2- + 2e^- + 4H^+ <--> Zn(s) + 4H2O
+'deltaH_VI': constants_deltaG['deltaG_VI'] + constants['T']*constants_deltaS['deltaS_VI'],      # ZnO + 2e^- + 2H^+ <--> Zn + H2O
+'deltaH_VIII': constants_deltaG['deltaG_VIII'] - constants['T']*constants_deltaS['deltaS_VIII'],# [J/mol] - Zn^2+ + 2OH^- <--> Zn(OH)2
+'deltaH_IX': constants_deltaG['deltaG_IX'] - constants['T']*constants_deltaS['deltaS_IX'],      # [J/mol] - Zn(OH)2 + OH^- <--> Zn(OH)3^-
+'deltaH_X': constants_deltaG['deltaG_X'] - constants['T']*constants_deltaS['deltaS_X'],         # [J/mol] - Zn(OH)3^- + OH^- <--> Zn(OH)4^-2
+'deltaH_XI': constants_deltaG['deltaG_XI'] - constants['T']*constants_deltaS['deltaS_XI']       # [J/mol] - Zn(OH)2 +2OH^- <--> Zn(OH)4^2-
+}
+
+# Molar heat capacity for reaction
+constants_deltaCp = {
+'deltaCp_I': constants_Cp['Cp_Zn'][0] - constants_Cp['Cp_Zn^2+'],                                                           # Zn^2+ + 2e^- <--> Zn(s)                   ----- There are two more coefficients in Cp for Zn that must be considered when taking the integral over temperature
+'deltaCp_II': constants_Cp['Cp_Zn'][0] + constants_Cp['Cp_H2O'] - (constants_Cp['Cp_Zn(OH)^+']) + constants_Cp['Cp_H^+'],      # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O     ----- There are two more coefficients in Cp for Zn that must be considered when taking the integral over temperature
+'deltaCp_III': constants_Cp['Cp_Zn'][0] + 2*constants_Cp['Cp_H2O'] - (constants_Cp['Cp_Zn(OH)2']+ 2*constants_Cp['Cp_H^+']),   # Zn(OH)^+ + 2e- + H^+ <--> Zn(s) + H2O     ----- There are two more coefficients in Cp for Zn that must be considered when taking the integral over temperature
+'deltaCp_IV': constants_Cp['Cp_Zn'][0] + 3*constants_Cp['Cp_H2O'] - (constants_Cp['Cp_Zn(OH)3^-'] + 3*constants_Cp['Cp_H^+']), # Zn(OH)2 + 2e^- + 2H^+ <--> Zn(s) + 2H2O   ----- There are two more coefficients in Cp for Zn that must be considered when taking the integral over temperature
+'deltaCp_V': constants_Cp['Cp_Zn'][0] + 4*constants_Cp['Cp_H2O'] - (constants_Cp['Cp_Zn(OH)4^2-'] + 4*constants_Cp['Cp_H^+']), # Zn(OH)4^2- + 2e^- + 4H^+ <--> Zn(s) + 4H2O----- There are two more coefficients in Cp for Zn that must be considered when taking the integral over temperature
+'deltaCp_VI': constants_Cp['Cp_Zn'][0] + constants_Cp['Cp_H2O'] - (constants_Cp['Cp_ZnO'][0]) + constants_Cp['Cp_H^+'],           # ZnO + 2e^- + 2H^+ <--> Zn + H2O           ----- There are two more coefficients in Cp for Zn that must be considered when taking the integral over temperature
+'deltaCp_VIII': constants_Cp['Cp_Zn(OH)2'] - (constants_Cp['Cp_Zn^2+'] + 2*constants_Cp['Cp_OH^-']),  # [J/mol] - Zn^2+ + 2OH^- <--> Zn(OH)2
+'deltaCp_IX': constants_Cp['Cp_Zn(OH)3^-'] - (constants_Cp['Cp_Zn(OH)2'] + constants_Cp['Cp_OH^-']),  # [J/mol] - Zn(OH)2 + OH^- <--> Zn(OH)3^-
+'deltaCp_X': constants_Cp['Cp_Zn(OH)4^2-'] - (constants_Cp['Cp_Zn(OH)3^-'] + constants_Cp['Cp_OH^-']),# [J/mol] - Zn(OH)3^- + OH^- <--> Zn(OH)4^-2
+'deltaCp_XI': constants_Cp['Cp_Zn(OH)4^2-'] - (constants_Cp['Cp_Zn(OH)2'] + 2*constants_Cp['Cp_OH^-'])# [J/mol] - Zn(OH)2 +2OH^- <--> Zn(OH)4^2-    
+}
+
+## Calculation of Standard reduction potentials for reactions
 # Zn^2+ + 2e^- <--> Zn(s)
 EI_0 = -(constants_deltaG_formation['deltaG_Zn'] - constants_deltaG_formation['deltaG_Zn^2+'])/(constants['n']*constants['F'])
 
@@ -32,7 +131,7 @@ EI_0 = -(constants_deltaG_formation['deltaG_Zn'] - constants_deltaG_formation['d
 EII_0 = -(constants_deltaG_formation['deltaG_Zn'] + constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)^+']) + constants_deltaG_formation['deltaG_H^+'])/(constants['n']*constants['F'])
 
 # Zn(OH)2 + 2e^- + 2H^+ <--> Zn(s) + 2H2O
-EIII_0 = -(constants_deltaG_formation['deltaG_Zn'] + 2*constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)2']+ 2*constants_deltaG_formation['deltaG_H^+']))/(constants['n']*constants['F'])
+EIII_0 = -(constants_deltaG_formation['deltaG_Zn'] + 2*constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)2-eps']+ 2*constants_deltaG_formation['deltaG_H^+']))/(constants['n']*constants['F'])
 
 # Zn(OH)3^- + 2e^- + 3H^+ <--> Zn(s) + 3H2O
 EIV_0 = -(constants_deltaG_formation['deltaG_Zn'] + 3*constants_deltaG_formation['deltaG_H2O'] - (constants_deltaG_formation['deltaG_Zn(OH)3^-'] + 3*constants_deltaG_formation['deltaG_H^+']))/(constants['n']*constants['F'])
@@ -55,14 +154,6 @@ constants_E0 = {
 'EOER_0': 1.229    # [V] - OER
 }
 
-# deltaG for reactions
-constants_deltaG = {
-'deltaG_VIII': constants_deltaG_formation['deltaG_Zn(OH)2'] - (constants_deltaG_formation['deltaG_Zn^2+'] + 2*constants_deltaG_formation['deltaG_OH^-']),  # [J/mol] - Zn^2+ + 2OH^- <--> Zn(OH)2
-'deltaG_IX': constants_deltaG_formation['deltaG_Zn(OH)3^-'] - (constants_deltaG_formation['deltaG_Zn(OH)2'] + constants_deltaG_formation['deltaG_OH^-']),  # [J/mol] - Zn(OH)2 + OH^- <--> Zn(OH)3^-
-'deltaG_X': constants_deltaG_formation['deltaG_Zn(OH)4^2-'] - (constants_deltaG_formation['deltaG_Zn(OH)3^-'] + constants_deltaG_formation['deltaG_OH^-']),# [J/mol] - Zn(OH)3^- + OH^- <--> Zn(OH)4^-2
-'deltaG_XI': constants_deltaG_formation['deltaG_Zn(OH)4^2-'] - (constants_deltaG_formation['deltaG_Zn(OH)2'] + 2*constants_deltaG_formation['deltaG_OH^-'])# [J/mol] - Zn(OH)2 +2OH^- <--> Zn(OH)4^2-
-}
-
 # The pZn where the equilibrium Zn(OH)2 <--> Zn(OH)3^-1 and Zn(OH)3^-1 <--> Zn(OH)4^2- becomes the same and the domain for Zn(OH)3^- vanishes
 pZn_threshold = (constants_deltaG['deltaG_IX']-constants_deltaG['deltaG_X'])/(constants['R']*constants['T']*np.log(10))
 
@@ -80,7 +171,7 @@ plt.plot(pH,EHER, '--')     # Line for the HER
 plt.plot(pH,EOER, '--')     # Line for the OER
 
 # Concentration of Zn ions
-pZn_values = np.array([5, 6])           # Values we iterate through for the activity of dissolved Zn - pZn = -log(c_Zn) --- Maximum value is 6 and minimum value is 0
+pZn_values = np.array([4, 6])           # Values we iterate through for the activity of dissolved Zn - pZn = -log(c_Zn) --- Maximum value is 6 and minimum value is 0
 pZn_values_dict = {'pZn': pZn_values}   # Dictionary with values
 linestyle = ['-', '--']
 
@@ -149,7 +240,7 @@ for i in range(len(pZn_values)):
         # Plotting the lines of the Pourbaix diagram
         plt.plot(pH[:index_I_III], EI[:index_I_III], 'k', label='Zn$^{2+}$ - Zn', linestyle=linestyle[i])                               # Electrochemical - Equilibrium between Zn^2+ and Zn(s) 
         plt.vlines(pHVIII, EI[index_I_III], 1.5, 'k', label='Zn$^{2+}$ - Zn(OH)_{2}', linestyle=linestyle[i])                           # Chemical        - Equilibrium between Zn^2+ and Zn(OH)2
-        plt.plot(pH[index_I_III: index_III_V], EIII[index_I_III: index_III_V], 'c', label='Zn(OH)$_{2}$ - Zn')                          # Electrochemical - Equilibrium between Zn(OH)2 and Zn
+        
         
 
         # Adding pZn text --> Should be adjusted later
@@ -163,12 +254,13 @@ for i in range(len(pZn_values)):
             plt.text(pH[index_III_IV+10], 1, pZn_string, color='r', rotation = 90)  # Adding pZn value to equilibrium between Zn(OH)2 and Zn(OH)3^- -- must be fixed
             plt.text(pH[index_IV_V+10], 1, pZn_string, color='m', rotation = 90)    # Adding pZn value to equilibrium between Zn(OH)3^-1 and Zn(OH)4^2 -- must be fixed
             plt.plot(pH[index_IV_V:], EV[index_IV_V:], 'm', label='Zn(OH)$_{4}^{2-}$ - Zn', linestyle=linestyle[i])                         # Electrochemical - Equilibrium between Zn(OH)4^2- and Zn   
+            plt.plot(pH[index_I_III: index_III_IV], EIII[index_I_III: index_III_IV], 'c', label='Zn(OH)$_{2}$ - Zn')                          # Electrochemical - Equilibrium between Zn(OH)2 and Zn
             
         elif pZn_values[i]<pZn_threshold:
             plt.plot(pH[index_III_V:], EV[index_III_V:], 'm', label='Zn(OH)$_{4}^{2-}$ - Zn', linestyle=linestyle[i])                       # Electrochemical - Equilibrium between Zn(OH)4^2- and Zn
             plt.vlines(pHXI, EV[index_III_V], 1.5, 'm', label='Zn(OH)$_{2}$ - Zn(OH)$_{4}^{2-}$', linestyle=linestyle[i])                   # Chemical        - Equilibrium between Zn(OH)2 and Zn(OH)4^2-
             plt.text(pH[index_III_V+10], 1, pZn_string, color='m', rotation = 90)    # Adding pZn value to equilibrium between Zn(OH)3^-1 and Zn(OH)4^2 -- must be fixed
-            
+            plt.plot(pH[index_I_III: index_III_V], EIII[index_I_III: index_III_V], 'c', label='Zn(OH)$_{2}$ - Zn')                          # Electrochemical - Equilibrium between Zn(OH)2 and Zn
 
         if i == len(pZn_values)-1:
             # Adding text to different domains --> Should be adjusted later
